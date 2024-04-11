3321d84d273ff0499ab097e7d83a2836e8eb1260
TOXIC-krot <toxic_toxich@mail.ru>
2024-04-11 18:14:58 +0500

    mainButtonClicke

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import './App.css'
 import Card from './components/card/Card';
 import Cart from './components/cart/Cart';
@@ -44,6 +44,16 @@ function App() {
     telegram.MainButton.text = "Sotib olish";
     telegram.MainButton.show();
   }
+
+  const onSendData = useCallback(() => {
+    telegram.sendData(JSON.stringify(cartItems));
+  }, [cartItems]);
+
+  useEffect(() => {
+    telegram.onEvent('mainButtonClicked', onSendData);
+
+    return () => telegram.offEvent('mainButtonClicked', onSendData)
+  }, [onSendData]);
   
   return (
     <>
