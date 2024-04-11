fca0a78ff3bb8051dc95edb1bf771ad0653e939a
TOXIC-krot <toxic_toxich@mail.ru>
2024-04-11 17:50:32 +0500

    push

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,9 @@
+import './Button.css'
+
+function Button(props) {
+  const {type, title, onClick, disable} = props
+
+  return <button className={`btn ${(type === 'add' && 'add' || type === 'remove' && 'remove' || type === 'checkout' && 'checkout')} ${disable === true && 'disabled'}`} disable={disable} onClick={onClick}>{title}</button>
+}
+
+export default Button
---
+++ src/constans/db.js
@@ -0,0 +1,28 @@
+export function getData() {
+    return [{
+        id: 1,
+        title: 'HTML',
+        price: 25.000,
+        Image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/800px-HTML5_logo_and_wordmark.svg.png'
+    }, {
+        id: 2,
+        title: 'CSS',
+        price: 39.000,
+        Image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/800px-CSS3_logo.svg.png'
+    }, {
+        id: 3,
+        title: 'BOOTSTRAP 5',
+        price: 15.000,
+        Image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/800px-Bootstrap_logo.svg.png'
+    }, {
+        id: 4,
+        title: 'Java Script',
+        price: 50.000,
+        Image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png'
+    }, {
+        id: 5,
+        title: 'React JS',
+        price: 100.000,
+        Image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/React_Logo_SVG.svg/1200px-React_Logo_SVG.svg.png'
+    }, ];
+}
